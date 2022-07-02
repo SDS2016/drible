b3d9c1bcff627c720c96855660faabf9525bdea8
SDS_eric <eric25990@yahoo.com>
2022-07-02 18:45:33 -0400

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Text>nope</Text>
+    <StatusBar style="auto" />
+  </View>
   );
 }
 
@@ -18,3 +21,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
